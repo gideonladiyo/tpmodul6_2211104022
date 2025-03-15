d4351c3f9ddd89f565f8b51c169e33d379900f5c
Gideon Ladiyo <alwaysfun0604@gmail.com>
2025-03-15 13:52:07 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_2211104022/Program.cs
+++ tpmodul6_2211104022/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        // random int 5 digits
+        this.id = random.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+    public void IncreasePlayCount(int count)
+    {
+        playCount += count;
+    }
+    public void PrintVideoDetail()
+    {
+        Console.WriteLine($"ID: {id}, Title: {title}, Play count: {playCount}");
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial design by contract - Gideon Toranawa Ladiyo");
+        video.PrintVideoDetail();
+    }
+}
